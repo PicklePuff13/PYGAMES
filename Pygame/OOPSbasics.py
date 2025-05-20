ee2c166cec59e2bfeb57ea54a4db44098df039a8
PicklePuff13 <puffkinpickle@gmail.com>
2025-05-20 18:16:27 +0100

    Update OOPSbasics.py

--- Pygame/OOPSbasics.py
+++ Pygame/OOPSbasics.py
@@ -4,10 +4,15 @@ class person:
     name="Abi"
     age=16
     height=150
+    #constructor-to initialise the values
+    def __init__(self,name,age,height):
+        self.name=name
+        self.age=age
+        self.height=height
 
     def user(self):
         self.name=input("What's your name?")
-        self.age=input("What's your height?")
+        self.age=input("What's your age?")
         self.height=input("What's your height?")
     
     def out(self):
@@ -15,7 +20,12 @@ class person:
         print(self.age)
         print(self.height)
     
-obj1=person()
-print(obj1.name)
+obj1=person("Daisy",13,122)
+obj2=person("Stuti",34,100)
+obj3=person("Belle",12,99)
+obj1.out()
+obj2.out()
+obj3.out()
 obj1.user()
 obj1.out()
+
