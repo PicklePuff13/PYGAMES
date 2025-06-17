a06bc51baf7c3f094e73597271c94cd10ba42b52
PicklePuff13 <puffkinpickle@gmail.com>
2025-06-17 18:24:23 +0100

    Update bouncing ball.py

--- bouncing ball.py
+++ bouncing ball.py
@@ -25,14 +25,34 @@ class Bouncyballs ():
         pos=(self.startx,self.starty)
         screen.draw.filled_circle(pos,self.radius, Colour)
 #object for class
-ball=Bouncyballs(450,100,55)
+ball1=Bouncyballs(350,75,37)
+ball2=Bouncyballs(450,100,55)
+ball3=Bouncyballs(550,125,65)
+bouncies=[ball1,ball2,ball3]
 def draw():
-    ball.draw()
+    for ball in bouncies:
+      ball.draw()
 def update(t):
     pass
     #constant acceleration 
-    uy=ball.vy
-    ball.vy+=Gravity*t
-    ball.starty+=(uy+ball.vy)*0.5*t
+    for ball in bouncies:
+      uy=ball.vy
+      ball.vy+=Gravity*t
+      ball.starty+=(uy+ball.vy)*0.5*t
+
+    #detecting and handling bounce
+      if ball.starty>695:
+          ball.starty=695
+          ball.vy=-ball.vy*0.9
+      ball.startx+=ball.vx*t
+    #restricting left and right edges
+      if ball.startx>695 or ball.startx<55:
+          ball.vx=-ball.vx
+
+def on_key_down(key):
+    if key==keys.SPACE:
+        ball1.vy=-850
+        ball2.vy=-850
+        ball3.vy=-850
 
 pgzrun.go()
