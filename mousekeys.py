a02d60a3fa5c28a365a5d95c11e1dd1e640273ee
PicklePuff13 <puffkinpickle@gmail.com>
2025-07-01 18:46:53 +0100

    Update mousekeys.py

--- mousekeys.py
+++ mousekeys.py
@@ -12,11 +12,55 @@ screen.fill(c5)
 
 pygame.display.update()
 
+class circles():
+    def __init__(self,colour,centre,radius,width=0):
+        self.colour=colour
+        self.centre=centre
+        self.radius=radius
+        self.width=width
+        self.screen=screen
+
+    def cirdraw(self):
+        pygame.draw.circle(self.screen,self.colour,self.centre,self.radius,self.width)
+    
+    def cirgrow(self,growf):
+        self.radius+=growf
+        pygame.draw.circle(self.screen,self.colour,self.centre,self.radius,self.width)
+
+
+
+
+#object creation
+circle1=circles(c6,(325,325),97)
+circle2=circles(c2,(325,325),77)
+circle3=circles(c1,(325,325),67)
+circle4=circles(c4,(325,325),57)
+
 run=True
 
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+        elif event.type==pygame.MOUSEBUTTONDOWN:
+            circle1.cirdraw()
+            circle2.cirdraw()
+            circle3.cirdraw()
+            circle4.cirdraw()
+            pygame.display.update()
+        elif event.type==pygame.MOUSEBUTTONUP:
+            circle1.cirgrow(10)
+            circle2.cirgrow(10)
+            circle3.cirgrow(10)
+            circle4.cirgrow(10)
+            pygame.display.update()
+        elif event.type==pygame.MOUSEMOTION:
+            mousecord=pygame.mouse.get_pos()
+            circlen=circles(c3,(mousecord),9)
+            circlen.cirdraw()
+            pygame.display.update()
+            
+
+        
 
 pygame.quit()
